0b49a1c51953b88ecedc60683a1f1774bb33a643
Ljaine Hossam Elsayed Abdelsamad <lujaine.hossam25@gmail.com>
2025-07-20 12:43:56 +0300

    added the get task by ID function , the error handling of creating a new task function , the .env file

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,27 +1,11 @@
 const express = require('express');
-const router = express.Router(); //to create a mini Express app just for task routes
+const router = express.Router();
+const taskController = require('../controllers/taskController');
 
-
-// Import controller functions
-const {
-  getAllTasks,
-  createTask,
-  updateTask,
-  deleteTask
-} = require('../controllers/taskController');
-
-// Define routes and link them to controller functions
-
-// GET /tasks → get all tasks
-router.get('/tasks', getAllTasks);
-
-// POST /tasks → create a new task
-router.post('/tasks', createTask);
-
-// PUT /tasks/:id → update a task by ID
-router.put('/tasks/:id', updateTask);
-
-// DELETE /tasks/:id → delete a task by ID
-router.delete('/tasks/:id', deleteTask);
+router.get('/', taskController.getAllTasks);
+router.get('/:id', taskController.getTaskById);
+router.post('/', taskController.createTask);
+router.put('/:id', taskController.updateTask);
+router.delete('/:id', taskController.deleteTask);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,13 @@
-const express = require('express'); // 1. Import express
-const app = express();               // 2. Create the app
-const taskRoutes = require('./routes/taskRoutes'); // 3. Import the router
+const express = require('express');
+const app = express();
+const taskRoutes = require('./routes/taskRoutes');
 
 
+require('dotenv').config();
+app.use(express.json());
+app.use('/tasks', taskRoutes);
 
-app.use(express.json()); // 4. Load JSON middleware
-
-
-app.use('/', taskRoutes); // 5. Register the routes
-
-// Start the server
-const PORT = process.env.PORT || 3000;  // 6. Choose a port
-app.listen(PORT, () => {                // 7. Start the server
-  console.log(` Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 3000 ;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
- 
